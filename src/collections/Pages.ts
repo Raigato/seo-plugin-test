44666eaa9210f961b06b11a402cbfd007de61d83
Raigato <gabriel.raymondou@swile.co>
2025-01-14 10:17:26 +0100

    chore: add example

---
+++ src/collections/Pages.ts
@@ -0,0 +1,20 @@
+import { CollectionConfig } from 'payload'
+
+export const Pages: CollectionConfig = {
+  slug: 'pages',
+  access: {
+    read: () => true,
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'slug',
+      type: 'text',
+      required: true,
+    },
+  ],
+}
